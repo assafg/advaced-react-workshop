dd6c879f5de113d0ed24d2c5c91b91902244f61e
Assaf Gannon <assaf.gannon@gmai.com>
2017-03-28 15:55:16 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Router, Route, hashHistory } from 'react-router';
 import Search from './search/Search';
+import Show from './show/Show';
+import Actor from './actor/Actor';
 
 class App extends React.Component {
   render () {
     return (
       <Router history={hashHistory}>
         <Route path="/search" component={Search} />
+        <Route path="/show" component={Show} />
+        <Route path="/actor" component={Actor} />
       </Router>
     );
   }
