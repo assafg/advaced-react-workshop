d284859043d4230c05dded64c9cb437973fe332d
Assaf Gannon <assaf.gannon@gmai.com>
2017-04-05 11:52:21 +0300

    refactored to add basic container

--- src/LoginToggle.js
+++ src/LoginToggle.js
@@ -1,6 +1,4 @@
 import React, { PropTypes } from 'react';
-import { connect } from 'react-redux';
-import { toggleIsLoggedIn } from './redux';
 
 const LoginToggle = (props) => (
   <div className="level-right">
@@ -26,12 +24,4 @@ LoginToggle.propTypes = {
   toggleIsLoggedIn: PropTypes.func,
 }
 
-const mapStateToProps = (state) => ({
-  isLoggedIn: state.isLoggedIn
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  toggleIsLoggedIn: () => dispatch(toggleIsLoggedIn())
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(LoginToggle);
+export default LoginToggle;
