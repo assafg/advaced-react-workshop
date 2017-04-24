9743a43b3d1fc14755be48a46936efcf8b43deb7
Assaf Gannon <assaf.gannon@gmai.com>
2017-04-24 12:03:12 +0300

    added jest-enzyme

--- src/search/results/tile.spec.js
+++ src/search/results/tile.spec.js
@@ -21,9 +21,14 @@ it('shallow renders without crashing', () => {
   const item = {
     id: '123',
     img: 'img-url',
-    title: 'test title',
+    name: 'test title',
     desc: 'tile test description',
   };
   const onSelect = () => {};
   const wrapper = shallow(<Tile item={item} onSelect={onSelect} />);
+  expect(wrapper.find('img').length).toBe(1);
+  expect(wrapper.find('.title').length).toBe(1);
+  expect(wrapper.find('.title').text()).toBe('test title');
+  expect(wrapper.find('.description').length).toBe(1);
+  expect(wrapper.find('.description').text()).toBe('tile test description');
 });
