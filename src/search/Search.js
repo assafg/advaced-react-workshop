f60df50ed67e5e336bc624b5b78cb0528a87acaa
Assaf Gannon <assaf.gannon@gmai.com>
2017-04-24 10:20:29 +0300

    added jest-enzyme

--- src/search/Search.js
+++ src/search/Search.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Search.css';
 import Header from './header/Header';
 import Results from './results/Results';
--- src/search/results/tile.spec.js
+++ src/search/results/tile.spec.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import Tile from './Tile';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -12,4 +14,16 @@ it('renders without crashing', () => {
   };
   const onSelect = () => {};
   ReactDOM.render(<Tile item={item} onSelect={onSelect} />, div);
+});
+
+
+it('shallow renders without crashing', () => {
+  const item = {
+    id: '123',
+    img: 'img-url',
+    title: 'test title',
+    desc: 'tile test description',
+  };
+  const onSelect = () => {};
+  const wrapper = shallow(<Tile item={item} onSelect={onSelect} />);
 });
